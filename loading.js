d909b30a2595c56be85e4fa48a5a7fe7ee4bb408
chethtra <35042023+chethtra@users.noreply.github.com>
2018-11-02 01:47:42 -0400

    init

---
+++ loading.js
@@ -0,0 +1,59 @@
+setLoadingStyle = function(styling, element){
+    let {height, width, margin, padding} = styling;
+    
+    element.style.height = height;
+    element.style.width = width;
+    element.style.margin = margin || 0;
+    element.style.padding = padding;
+    element.style.borderRadius = styling['border-radius'];
+    element.style.backgroundColor = 'gray';
+
+    return element;
+}
+
+removeLoading = function(){
+    let loading = document.getElementById('loading');
+    document.body.removeChild(loading);
+}
+
+startLoadingTimer = function(duration){
+    setTimeout(()=>removeLoading(), duration);
+}
+
+createLoadingElements = function(elements){
+    elements.forEach(e => { 
+
+        let elementStyles = window.getComputedStyle(e);
+        
+        let loadingDiv = document.createElement('div');
+        loadingDiv = setLoadingStyle(elementStyles, loadingDiv);
+        document.getElementById('loading').appendChild(loadingDiv);
+    });
+}
+
+class LoadingScreen{
+
+    constructor(container, duration = 0){
+        this.duration = duration;
+        this.loadingContainer = container;   
+    }
+
+    start(){
+        let children = document.querySelector(this.loadingContainer).childNodes;
+        children = Array.from(children);
+
+        let loadableElements = children.filter(e => e.scrollHeight && e.scrollWidth);
+
+        createLoadingElements(loadableElements);
+
+        if(this.duration)
+            startLoadingTimer(this.duration);     
+    }
+
+    dismiss(){
+        removeLoading();
+    }
+}
+
+let load = new LoadingScreen('#container');
+load.start();
